42495ad6341d04ce55a9cd4be15d87dd4c33be92
J. Patrick Connor <patrickconnor@college.harvard.edu>
2026-03-19 22:29:49 -0400

    LLM text review first commit

---
+++ COMPASS/ERIS_testing/LLM_extraction/compile_text_for_LLM_review.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+DATA_PATH = '/data/gusev/USERS/jpconnor/data/CAIA/COMPASS/'
+baca_df = pd.read_csv(
+    os.path.join(DATA_PATH, 'baca_lab_patient_annotations.tsv'),
+    sep="\t",
+    encoding="utf-8",
+    encoding_errors="replace")
+text_df = pd.read_csv(os.path.join(DATA_PATH, 'prostate_text_data.csv'))
+
+just_NEPC_baca_df = baca_df.loc[~baca_df['NEPC_DIAG_DATE'].isna()]
+just_NEPC_baca_df['TIME_BETWEEN_MED_AND_NEPC_DIAG'] = np.abs(pd.to_datetime(just_NEPC_baca_df['MEDICATION_START_TIME']) - pd.to_datetime(just_NEPC_baca_df['NEPC_DIAG_DATE']))
+just_NEPC_baca_df['TIME_BETWEEN_MED_AND_NEPC_DIAG'] = just_NEPC_baca_df['TIME_BETWEEN_MED_AND_NEPC_DIAG'].apply(lambda x : x.days)
+plt.hist(just_NEPC_baca_df['TIME_BETWEEN_MED_AND_NEPC_DIAG'])
+
+candidate_patients = baca_df.loc[baca_df['Inidcation of Platinum Therapy'].isna(), 
+                                 ['DFCI_MRN', 'PLATINUM_CHEMO_MED', 'MEDICATION_START_TIME']]
+LLM_text_df = text_df.merge(candidate_patients, on='DFCI_MRN', how='inner')
+event_dt = pd.to_datetime(LLM_text_df['EVENT_DATE'], errors='coerce', utc=True)
+med_dt = pd.to_datetime(LLM_text_df['MEDICATION_START_TIME'], errors='coerce', utc=True)
+
+LLM_text_df['NOTE_TIME_REL_PLATINUM_START'] = event_dt - med_dt
+LLM_text_df['NOTE_TIME_REL_PLATINUM_START'] = LLM_text_df['NOTE_TIME_REL_PLATINUM_START'].apply(lambda x : x.days)
+candidate_LLM_text_df = LLM_text_df.loc[np.abs(LLM_text_df['NOTE_TIME_REL_PLATINUM_START']) <= 90]
